02ec0431cf4b590112242e70a0c2fd8514cf1ab2
stanley <stanley.njuguna@student.moringaschool.com>
2024-01-14 09:49:40 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+
+document.addEventListener("DOMContentLoaded", function (){
+    console.log("The DOM has loaded")
+    content();
+ })
+ 
+ const content = () => {
+     const textToShow = document.getElementById("text");
+     textToShow.textContent= "This is really cool!";
+ 
+ };
